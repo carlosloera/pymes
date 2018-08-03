c4874273a1e7de6e3187a044d294f341dfb0b5e2
carlos <carlos_ivan_loera@hotmail.com>
2018-08-03 11:10:15 -0500

    integracion 1.2

--- app/Http/Controllers/answerController.php
+++ app/Http/Controllers/answerController.php
@@ -12,6 +12,7 @@ class answerController extends Controller
     
 
     public function store(Request $request){
+         
         //dd(sizeof($request->answer));
          //dd($request)
         //return response()->json("hola");
@@ -21,6 +22,7 @@ class answerController extends Controller
                 //dd($request->answer_.strval($i));
                 //dd($request->id_question_.$a);
                 */
+        /*        
             dd($request);
             //dd($request->answer[1]);
 
@@ -85,7 +87,26 @@ class answerController extends Controller
                     }
                 }
             }   
-            
+            */ 
+            $size = sizeof($request->answer);
+             for($i=0; $i<$size; $i++){
+                $exist;
+                $exist = Answer::where('id_question',$request->id_question[$i])->first();
+                //dd($exist);
+                if($exist){
+                    $exist->answer = $request->answer[$i];   
+                    //dd($exist);
+                    $exist->save();
+                }
+                else{
+                    //dd("no");
+                    $answer = new Answer;
+                    $answer->answer = $request->answer[$i];
+                    $answer->id_question = $request->id_question[$i];
+                    $answer->id_periods = $request->process_id;
+                    $answer->save();
+                }
+            }   
             //$request->session()-flash('status','Guardado Correctamente');
             return back()->with('status', 'Guardado Correctamente');
 
